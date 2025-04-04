aaa7132ccc250222665ab4332116e2d66392cfd9
alenomarbaev <alen.omarbayev2005@gmail.com>
2025-04-04 12:15:50 +0500

    main, login, about us pages

---
+++ carMarketplace/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
